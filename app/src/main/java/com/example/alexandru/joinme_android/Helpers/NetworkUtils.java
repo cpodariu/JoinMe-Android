1037278b1f316d8c6e0eb62838f0c23d0f5dc6d8
cpodariu <podariucatalin97@gmail.com>
2017-11-18 16:54:07 +0200

    added networkutils

--- app/src/main/java/com/example/alexandru/joinme_android/Helpers/NetworkUtils.java
+++ app/src/main/java/com/example/alexandru/joinme_android/Helpers/NetworkUtils.java
@@ -1,8 +1,26 @@
 package com.example.alexandru.joinme_android.Helpers;
 
+import android.net.Uri;
+
 /**
  * Created by cpodariu on 11/18/17.
  */
 
 public class NetworkUtils {
+    private static final String PROTOCOL = "http";
+    private static final String BASE_URL = "192.168.43.253:8080";
+    private static final String LOG_IN_PATH = "/rest/login";
+    private static final String EMAIL_KEY = "email";
+    public static final String PASSWORD_KEY = "password";
+
+    public static String buildLogInUrl(String email, String password)
+    {
+        Uri.Builder builder = new Uri.Builder();
+        builder.scheme(PROTOCOL)
+                .encodedAuthority(BASE_URL)
+                .appendEncodedPath(LOG_IN_PATH)
+                .appendQueryParameter(EMAIL_KEY, email)
+                .appendQueryParameter(PASSWORD_KEY, password);
+        return builder.build().toString();
+    }
 }
