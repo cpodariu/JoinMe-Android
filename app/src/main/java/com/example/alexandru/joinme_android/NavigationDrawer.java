faf18c3eabbd8351ece1057348255176505500e9
cpodariu <podariucatalin97@gmail.com>
2017-11-18 18:04:35 +0200

    ready for merge

--- app/src/main/java/com/example/alexandru/joinme_android/NavigationDrawer.java
+++ app/src/main/java/com/example/alexandru/joinme_android/NavigationDrawer.java
@@ -91,18 +91,8 @@ public class NavigationDrawer extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
+        if (id == R.id.get_map_fragment) {
+            this.setFragment(new MapFragment());
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
